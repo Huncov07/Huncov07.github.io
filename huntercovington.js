1d39bc071808ff015ddd390dd92b9b662e39ebee
Hunter-Covington <huncov07@byui.edu>
2020-05-03 14:22:32 -0600

    Second test

---
+++ huntercovington.js
@@ -0,0 +1,8 @@
+
+let day = new Date();
+let getYear = day.getFullYear();
+let lastModified = document.lastModified;
+
+
+document.getElementById('modifiedDate').innerHTML = lastModified;
+document.getElementById('currentYear').innerHTML = getYear;
