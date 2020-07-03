5c2e1f178c3e2dacf074282744724fcd0cb0ae33
Hunter-Covington <huncov07@byui.edu>
2020-07-03 11:01:58 -0600

    revamp website

--- js/huntercovington.js
+++
@@ -1,9 +0,0 @@
-let set = new Date(document.lastModified);
-let month = set.getMonth() + 1;
-let date = set.getDate();
-let year = set.getFullYear();
-let hour = set.getHours();
-let mintue = set.getMinutes();
-let second = set.getSeconds();
-let update = month + "/" + date + "/" + year + " " + hour + ":" + mintue + ":" + second;
-document.getElementById("update").innerHTML = update;
